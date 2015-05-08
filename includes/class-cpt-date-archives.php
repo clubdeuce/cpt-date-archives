0da1c5de2a03e3e7f30e1a6fde9a5fc55e6d3d60
Daryl Lozupone <daryl@clubduece.com>
2015-05-08 12:40:02 -0400

    #4: Add support for date based feeds as well Closes #4

--- includes/class-cpt-date-archives.php
+++ includes/class-cpt-date-archives.php
@@ -34,14 +34,20 @@ class CPT_Date_Archives {
 
 		foreach( $post_types as $post_type ) {
 			//Day archive
-			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{2})/([0-9]{2})/page/?([0-9]{1,})/?", 'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]$matches[3]&paged=$matches[4]','top');
-			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})",                 'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]$matches[3]', 'top' );
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{2})/([0-9]{2})/feed/(feed|rdf|rss|rss2|atom)/?$", 'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]$matches[3]&feed=$matches[4]','top');
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{2})/([0-9]{2})/(feed|rdf|rss|rss2|atom)/?$",      'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]$matches[3]&feed=$matches[4]','top');
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{2})/([0-9]{2})/page/?([0-9]{1,})/?$",             'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]$matches[3]&paged=$matches[4]','top');
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{2})/([0-9]{1,2})/?$",                             'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]$matches[3]', 'top' );
 			//Monthly archive
-			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{2})/page/?([0-9]{1,})/?",            'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]&paged=$matches[3]','top');
-			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{1,2})",                              'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]', 'top' );
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{2})/feed/(feed|rdf|rss|rss2|atom)/?$", 'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]&feed=$matches[3]','top');
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{2})/(feed|rdf|rss|rss2|atom)/?$",      'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]&feed=$matches[3]','top');
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{2})/page/?([0-9]{1,})/?$",             'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]&paged=$matches[3]','top');
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{2})/?$",                               'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]', 'top' );
 			//Yearly archive
-			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/page/?([0-9]{1,})/?",                       'index.php?post_type=' . $post_type->name . '&m=$matches[1]&paged=$matches[2]','top');
-			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})",                                           'index.php?post_type=' . $post_type->name . '&m=$matches[1]', 'top' );
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/feed/(feed|rdf|rss|rss2|atom)/?$", 'index.php?post_type=' . $post_type->name . '&m=$matches[1]&feed=$matches[2]','top');
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/(feed|rdf|rss|rss2|atom)/?$",      'index.php?post_type=' . $post_type->name . '&m=$matches[1]&feed=$matches[2]','top');
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/page/?([0-9]{1,})/?$",             'index.php?post_type=' . $post_type->name . '&m=$matches[1]&paged=$matches[2]','top');
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/?$",                               'index.php?post_type=' . $post_type->name . '&m=$matches[1]', 'top' );
 		}
 
 	}
