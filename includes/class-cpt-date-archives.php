74e3fd50b16a1a47225e2ea926d5679305abef5d
Daryl Lozupone <daryl@clubduece.com>
2015-05-08 11:17:42 -0400

    #5: Add pagination URLs Closes #5

--- includes/class-cpt-date-archives.php
+++ includes/class-cpt-date-archives.php
@@ -19,29 +19,29 @@ class CPT_Date_Archives {
 
 	public function __construct() {
 
-		add_action( 'init', array( $this, 'custom_rewrite_rules' ), 999 );
+		add_action( 'init', array( $this, 'custom_rewrite_rules' ) );
 		add_action( 'init', array( $this, 'settings_page' ) );
 
 	}
 
 	/**
-	 * Add rewrite rules for public custom post types to create day, month, and year archives
+	 * Add rewrite rules for supported custom post types to create day, month, and year archives
 	 */
 	public function custom_rewrite_rules() {
 
-		$post_types = get_post_types( array( 'public' => true, '_builtin' => false ), 'objects' );
 		$settings_page = CPT_Date_Archive_Settings::init();
 		$post_types    = $settings_page->get_post_type_objects();
 
 		foreach( $post_types as $post_type ) {
-			if ( $post_type->has_archive ) {
-				//Day archive
-				add_rewrite_rule( "^{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{1,2}/[0-9]{1,2})", 'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]$matches[3]', 'top' );
-				//Monthly archive
-				add_rewrite_rule( "^{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{1,2})",            'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]', 'top' );
-				//Yearly archive
-				add_rewrite_rule( "^{$post_type->rewrite['slug']}/([0-9]{4})",                         'index.php?post_type=' . $post_type->name . '&m=$matches[1]', 'top' );
-			}
+			//Day archive
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{2})/([0-9]{2})/page/?([0-9]{1,})/?", 'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]$matches[3]&paged=$matches[4]','top');
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{1,2})/([0-9]{1,2})",                 'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]$matches[3]', 'top' );
+			//Monthly archive
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{2})/page/?([0-9]{1,})/?",            'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]&paged=$matches[3]','top');
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/([0-9]{1,2})",                              'index.php?post_type=' . $post_type->name . '&m=$matches[1]$matches[2]', 'top' );
+			//Yearly archive
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})/page/?([0-9]{1,})/?",                       'index.php?post_type=' . $post_type->name . '&m=$matches[1]&paged=$matches[2]','top');
+			add_rewrite_rule( "{$post_type->rewrite['slug']}/([0-9]{4})",                                           'index.php?post_type=' . $post_type->name . '&m=$matches[1]', 'top' );
 		}
 
 	}
